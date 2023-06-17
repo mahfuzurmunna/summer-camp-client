4bc4f6ee3b9b6fcadd09e3bca4f3cb6f0affd680
mahfuzurmunna <mahfuzurrm07@gmail.com>
2023-06-17 17:36:00 +0600

    animation added scrool

--- Readme.md
+++ Readme.md
@@ -2,24 +2,24 @@
 
 ![Logo](https://i.ibb.co/2WXqLcM/logo.png)
 
-- Single page web Application
-- Individual dashboard access limit to student, instructor and admin
-- Admin can manage users and classes
-- Instrcutors can add and manage his/her classes
-- Student can see their selected class and pay or delete the selected classes
-- Login and register page with firebase
-- Login with google and email
-- Load data from cretaed api
-- private route
-- Mongo db database. (send real time data, load them in the website through api)
-- Added simple animations
-- CRUD operations
+- This is a single-page web application.
+- Each user has an individual dashboard with access limited according to their role as a student, -  instructor, or admin.
+- The admin has the authority to manage users and classes.
+- Instructors can add and manage their own classes.
+- Students can view their selected classes, make payments, and delete classes they have chosen.
+- The application includes a login and registration page using Firebase for authentication.
+- Users have the option to log in using Google or email.
+- Data is loaded from a created API.
+- Private routes ensure secure access.
+- The application utilizes a MongoDB database for real-time data and API integration.
+- Simple animations are implemented for a better user experience.
+- CRUD operations (Create, Read, Update, Delete) are supported.
 
 ## Used Packages
 
 These packages are used in the web app:
 
-"@tanstack/react-query": "^4.29.13",
+    -tanstack/react-query 
     - axios
     - daisyui
     - firebase
--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -5,57 +5,64 @@ import Banner from "./Banner";
 import ClassSection from "./ClassSection";
 import Instructor from "./Instructor";
 import { useQuery } from "@tanstack/react-query";
+import { motion, useScroll } from "framer-motion";
 
 const Home = () => {
   const [axiosSecure] = useAxios();
 
+  const { scrollYProgress } = useScroll();
   // eslint-disable-next-line no-unused-vars
-  const { data: users = []} = useQuery(["users"], async () => {
+  const { data: users = [] } = useQuery(["users"], async () => {
     const res = await axiosSecure.get("/allusers");
     return res.data;
   });
-   const { data: classes = [] } = useQuery(["classes"], async () => {
-     const res = await axiosSecure.get("/allclasses");
-     return res.data;
-   });
+  const { data: classes = [] } = useQuery(["classes"], async () => {
+    const res = await axiosSecure.get("/allclasses");
+    return res.data;
+  });
 
-   const approvedClass = classes.filter(cl => cl.status === 'approved');
+  const approvedClass = classes.filter((cl) => cl.status === "approved");
 
   const instructorUsers = users.filter((user) => user.role === "instructor");
 
   console.log(users);
   return (
-    <div>
-      {/* banner section */}
-      <Banner />
-      {/* about us section */}
-      <About />
-      {/* instructor section */}
-      <div className="lg:w-[1150px] w-full  mx-auto md:my-32 my-24 lg:my-44 text-center">
-        <p className="text-accent text-base mb-2 font-medium">OUR TEACHERS</p>
-        <h1 className=" text-4xl lg:text-[52px] leading-tight font-bold text-primary ">
-          Our Popular Instructors
-        </h1>
-        <div className="grid lg:grid-cols-3 gap-4">
-          {instructorUsers.slice(0, 6).map((user) => (
-            <Instructor key={user._id} user={user}></Instructor>
-          ))}
+    <>
+      <div className="relative">
+        <motion.div
+          className="progress-bar z-50"
+          style={{ scaleX: scrollYProgress }}
+        />
+        {/* banner section */}
+        <Banner />
+        {/* about us section */}
+        <About />
+        {/* instructor section */}
+        <div className="lg:w-[1150px] w-full  mx-auto md:my-32 my-24 lg:my-44 text-center">
+          <p className="text-accent text-base mb-2 font-medium">OUR TEACHERS</p>
+          <h1 className=" text-4xl lg:text-[52px] leading-tight font-bold text-primary ">
+            Our Popular Instructors
+          </h1>
+          <div className="grid lg:grid-cols-3 gap-4">
+            {instructorUsers.slice(0, 6).map((user) => (
+              <Instructor key={user._id} user={user}></Instructor>
+            ))}
+          </div>
+        </div>
+        {/* class section */}
+        <div className="lg:w-[1150px] w-full  mx-auto md:my-32 my-24 lg:my-44 text-center">
+          <p className="text-accent text-base mb-2 font-medium">OUR CLASSES</p>
+          <h1 className=" text-4xl lg:text-[52px] leading-tight font-bold text-primary ">
+            Our Music Classe
+          </h1>
+          <div className="grid lg:grid-cols-3 gap-4">
+            {approvedClass.slice(0, 6).map((cl) => (
+              <ClassSection key={cl._id} cl={cl}></ClassSection>
+            ))}
+          </div>
         </div>
       </div>
-
-      {/* class section */}
-      <div className="lg:w-[1150px] w-full  mx-auto md:my-32 my-24 lg:my-44 text-center">
-        <p className="text-accent text-base mb-2 font-medium">OUR CLASSES</p>
-        <h1 className=" text-4xl lg:text-[52px] leading-tight font-bold text-primary ">
-          Our Music Classe
-        </h1>
-        <div className="grid lg:grid-cols-3 gap-4">
-          {approvedClass.slice(0, 6).map((cl) => (
-            <ClassSection key={cl._id} cl={cl}></ClassSection>
-          ))}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
